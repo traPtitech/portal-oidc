358b5550257a16467bf044805b1398b9a3c4c1d6
saitenntaisei <saitenntaisei@gmail.com>
2025-08-23 19:45:25 +0900

    fix: correct UUID variable usage and improve JTI handling logic

--- pkg/usecase/store/store.go
+++ pkg/usecase/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/cockroachdb/errors"
@@ -9,6 +10,7 @@ import (
 	"github.com/ory/fosite"
 	"github.com/traPtitech/portal-oidc/pkg/domain"
 	"github.com/traPtitech/portal-oidc/pkg/domain/repository"
+	"github.com/traPtitech/portal-oidc/pkg/domain/store"
 )
 
 type Store struct {
@@ -19,23 +21,25 @@ func NewStore(repo repository.Repository) *Store {
 	return &Store{repo: repo}
 }
 
+var _ store.Store = &Store{}
+
 func (s *Store) GetClient(ctx context.Context, id string) (fosite.Client, error) {
 	// client := new(fosite.DefaultClient)
-	uuid, err := uuid.Parse(id)
+	client_id, err := uuid.Parse(id)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to parse UUID")
 	}
 
-	client, err := s.repo.GetOIDCClient(ctx, domain.ClientID(uuid))
+	client, err := s.repo.GetOIDCClient(ctx, domain.ClientID(client_id))
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to get client")
 	}
 	fositeClient := &fosite.DefaultClient{
-		ID:            uuid.String(),
+		ID:            client_id.String(),
 		Secret:        []byte(client.Secret),
 		RedirectURIs:  client.RedirectURIs,
-		GrantTypes:    []string{},
-		ResponseTypes: []string{},
+		GrantTypes:    []string{}, // refresh_token, authorization_code
+		ResponseTypes: []string{}, // code, code id_token
 	}
 	return fositeClient, nil
 
@@ -59,8 +63,12 @@ func (s *Store) SetClientAssertionJWT(ctx context.Context, jti string, after tim
 		return errors.Wrap(err, "Failed to delete old blacklisted JTI")
 	}
 
-	if blacklistedJTI, exists := s.repo.GetBlacklistJTI(ctx, jti); blacklistedJTI.JTI == jti && exists == nil {
+	_, err := s.repo.GetBlacklistJTI(ctx, jti)
+	if err != nil && err != sql.ErrNoRows {
 		return fosite.ErrJTIKnown
+	}
+	if err == nil {
+		return errors.New("JTI already exists")
 	}
 
 	if err := s.repo.CreateBlacklistJTI(ctx, jti, after); err != nil {
