b6210e4cca9af86e31ed55e7ac518503d5d9df1a
anko9801 <37263451+anko9801@users.noreply.github.com>
2026-01-08 14:31:23 +0900

    chore: add SQL queries for oidc and portal

---
+++ db/query/oidc.sql
@@ -0,0 +1,31 @@
+-- Client queries
+
+-- name: CreateClient :exec
+INSERT INTO clients (
+    client_id,
+    client_secret_hash,
+    name,
+    client_type,
+    redirect_uris
+) VALUES (?, ?, ?, ?, ?);
+
+-- name: GetClient :one
+SELECT * FROM clients WHERE client_id = ?;
+
+-- name: ListClients :many
+SELECT * FROM clients;
+
+-- name: UpdateClient :exec
+UPDATE clients SET
+    name = ?,
+    client_type = ?,
+    redirect_uris = ?
+WHERE client_id = ?;
+
+-- name: UpdateClientSecret :exec
+UPDATE clients SET
+    client_secret_hash = ?
+WHERE client_id = ?;
+
+-- name: DeleteClient :exec
+DELETE FROM clients WHERE client_id = ?;
