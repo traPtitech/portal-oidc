5c83ed18d7989e67f30ba20cae3ae41d7d2e247f
anko9801 <37263451+anko9801@users.noreply.github.com>
2026-01-26 00:53:33 +0900

    feat: add authorization_codes and tokens tables

--- internal/repository/oidc/querier.go
+++ internal/repository/oidc/querier.go
@@ -6,15 +6,34 @@ package oidc
 
 import (
 	"context"
+	"database/sql"
 )
 
 type Querier interface {
+	// Authorization Code queries
+	CreateAuthorizationCode(ctx context.Context, arg CreateAuthorizationCodeParams) error
 	// Client queries
 	CreateClient(ctx context.Context, arg CreateClientParams) error
+	// Token queries
+	CreateToken(ctx context.Context, arg CreateTokenParams) error
+	DeleteAllAuthorizationCodes(ctx context.Context) error
 	DeleteAllClients(ctx context.Context) error
+	DeleteAllTokens(ctx context.Context) error
+	DeleteAuthorizationCode(ctx context.Context, code string) error
 	DeleteClient(ctx context.Context, clientID string) error
+	DeleteExpiredAuthorizationCodes(ctx context.Context) error
+	DeleteExpiredTokens(ctx context.Context) error
+	DeleteToken(ctx context.Context, id string) error
+	DeleteTokenByAccessToken(ctx context.Context, accessToken string) error
+	DeleteTokenByRefreshToken(ctx context.Context, refreshToken sql.NullString) error
+	DeleteTokensByUserAndClient(ctx context.Context, arg DeleteTokensByUserAndClientParams) error
+	GetAuthorizationCode(ctx context.Context, code string) (AuthorizationCode, error)
 	GetClient(ctx context.Context, clientID string) (Client, error)
+	GetTokenByAccessToken(ctx context.Context, accessToken string) (Token, error)
+	GetTokenByID(ctx context.Context, id string) (Token, error)
+	GetTokenByRefreshToken(ctx context.Context, refreshToken sql.NullString) (Token, error)
 	ListClients(ctx context.Context) ([]Client, error)
+	UpdateAuthorizationCodePKCE(ctx context.Context, arg UpdateAuthorizationCodePKCEParams) error
 	UpdateClient(ctx context.Context, arg UpdateClientParams) error
 	UpdateClientSecret(ctx context.Context, arg UpdateClientSecretParams) error
 }
